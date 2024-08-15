e525182ca9a5943355a306fcc899bcaf4425d9ab
seanborg <sean_b@optimove.com>
2024-08-15 13:44:43 +0100

    WIP update record

--- app/database.py
+++ app/database.py
@@ -11,13 +11,13 @@ else:
     postgres_uri = ""
 
 
-async def psql_search_user(*params):
+async def psql_search_user(user_email):
     try:
         conn = await asyncpg.connect(postgres_uri)
 
         sql = "SELECT * FROM users WHERE email = $1"
         
-        result = await conn.fetchrow(sql, *params)
+        result = await conn.fetchrow(sql, user_email)
 
         return result
 
@@ -61,14 +61,30 @@ async def psql_create_record(record: Record, user_id: int):
 
 async def psql_get_record(
         user_id:int,
-        record_name:str
+        record_id: str = None,
+        record_name:str = None
     ):
+    '''
+    Get a record by name or by ID.
+    - Used as a search feature for users to get records by name
+    - Used to search for records by ID internally.
+    '''
     try:
         conn = await asyncpg.connect(postgres_uri)
 
-        sql = "SELECT * FROM record WHERE user_id = $1 AND name LIKE $2;"
+        sql = ""
 
-        result = await conn.fetch(sql, user_id, record_name)
+        record_identifier = None
+
+        if record_name is not None:
+            sql = "SELECT * FROM record WHERE user_id = $1 AND name LIKE $2;"
+            record_identifier = record_name
+
+        if record_id is not None:
+            sql = "SELECT * FROM record WHERE user_id = $1 AND id LIKE $2;"
+            record_identifier = record_id
+
+        result = await conn.fetch(sql, user_id, record_identifier)
 
         return result
     
@@ -79,11 +95,24 @@ async def psql_get_record(
             await conn.close()
 
 
+async def psql_update_record(record_id: int, user_id: int):
+    try:
+        conn = await asyncpg.connect(postgres_uri)
+
+        record = await psql_get_record(user_id=user_id, record_id=record_id)
+
+        sql = "UPDATE record SET name = $1, description = $2, username = $3, password = $4, folder_id = $5 WHERE id = $6;"
+
+        result = await conn.execute(sql, )
+    except Exception:
+        raise Exception("Failed to update record")
+
+
 async def psql_delete_record(record_id: int, user_id: int):
     try:
         conn = await asyncpg.connect(postgres_uri)
 
-        sql = "DELETE FROM record WHERE id = $1 AND user_id = $2 LIMIT 1"
+        sql = "DELETE FROM record WHERE id = $1 AND user_id = $2"
 
         result = await conn.execute(sql, record_id, user_id)
 
--- app/record.py
+++ app/record.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Header
 from typing import Annotated
 from app.validate import is_alphanumeric
 from app.user import authenticate_user
-from app.database import psql_create_record, psql_get_record, psql_delete_record, psql_search_user
+from app.database import psql_create_record, psql_get_record, psql_delete_record, psql_search_user, psql_update_record
 from models.records import Record
 
 password_router = APIRouter(prefix="/records", tags=["Records"])
@@ -56,7 +56,6 @@ async def get_record(
 
 @password_router.post("/update/{record_id}")
 async def update_record(
-    record: Record,
     record_id: int,
     auth_token: Annotated[str | None, Header(...)] = None,
     user_email: Annotated[str | None, Header(...)] = None
@@ -64,8 +63,9 @@ async def update_record(
     try:
         await authenticate_user(auth_token=auth_token, user_email=user_email)
 
-        # Update password in db if found
-        # await psql_create_record(password=password)
+        user = await psql_search_user(user_email)
+
+        record = await psql_update_record(record_id= record_id)
 
         return {"message":"success"}
     except Exception as e:
@@ -81,10 +81,15 @@ async def delete_record(
     try:
         await authenticate_user(auth_token=auth_token, user_email=user_email)
 
-        user_id = await psql_search_user(user_email)
+        user = await psql_search_user(user_email)
 
         # Make sure it is not possible for user to delete record they dont own
-        result = await psql_delete_record(record_id, user_id['id'])
+        await psql_delete_record(record_id, user['id'])
+
+        return {
+            "status": 200,
+            "message": "Deleted Record Successfully"
+        }
     except Exception as e:
         return {
             "status": 500,
